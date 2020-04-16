cbeb3d3d90309d47546dfc39d813c1a704477bed
Nizvan018 <56444748+Nizvan018@users.noreply.github.com>
2020-04-15 19:12:15 -0500

    Add files via upload

---
+++ EjercicioDos.py
@@ -0,0 +1,10 @@
+print("Introduce un número: ")
+num = int(input())
+resultado = 0
+
+if num <= 10000 and num > 0:
+    for i in range(1, num + 1):
+        resultado = resultado + i
+        print("El resultado es: ",resultado)
+else:
+    print("El número es inválido")
