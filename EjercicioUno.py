1a1adc39fe261a53befe2e0f8772e0890136d39a
Nizvan018 <56444748+Nizvan018@users.noreply.github.com>
2020-05-02 16:46:12 -0500

    Update EjercicioUno.py

--- EjercicioUno.py
+++ EjercicioUno.py
@@ -8,7 +8,4 @@ numDosPosicion = espacio
 numUno = int(cdn[:numUnoPosicion])
 numDos = int(cdn[numDosPosicion:])
 
-if numUno >= 0 and numDos <= 10:
-    print(numUno + numDos)
-else:
-    print("Los núeros son inválidos: A >= 0, B <= 10")
+print(numUno + numDos)
