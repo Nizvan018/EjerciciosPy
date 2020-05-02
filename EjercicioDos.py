50594dc8097b718fbd9577876d4c289b9508360b
Nizvan018 <56444748+Nizvan018@users.noreply.github.com>
2020-05-02 16:47:06 -0500

    Update EjercicioDos.py

--- EjercicioDos.py
+++ EjercicioDos.py
@@ -5,6 +5,10 @@ resultado = 0
 if num <= 10000 and num > 0:
     for i in range(1, num + 1):
         resultado = resultado + i
-        print("El resultado es: ",resultado)
+    print("El resultado es: ",resultado)
+elif num < 0:
+    for i in range(num, -1):
+        resultado = resultado + i
+    print("El resultado es: ", resultado)
 else:
     print("El número es inválido")
